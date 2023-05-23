c54e206899954168255c91ff2eecef8691efb311
Arteria <alenakuliskina@gmail.com>
2023-05-23 16:52:52 +0600

    [+] first settings

---
+++ PWASServer/src/main/java/com/pwas/PWASApplication.java
@@ -0,0 +1,11 @@
+package com.pwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PWASApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PWASApplication.class, args);
+    }
+}
